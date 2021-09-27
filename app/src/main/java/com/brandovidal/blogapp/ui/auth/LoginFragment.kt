82231b0337659f0612a9c497e92b869b918ff4d4
Brando Vidal <yisusdracon@gmail.com>
2021-09-26 23:48:35 -0500

    feat: create LoginFragment 1/2
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

---
+++ app/src/main/java/com/brandovidal/blogapp/ui/auth/LoginFragment.kt
@@ -0,0 +1,20 @@
+package com.brandovidal.blogapp.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.brandovidal.blogapp.R
+import com.brandovidal.blogapp.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+    private lateinit var binding: FragmentLoginBinding
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentLoginBinding.bind(view)
+    }
+}
